a9d3f4e2aaf7fe944bf9dc5bdb7577955215d69f
harshith-shiva <harshithshivab@gmail.com>
2026-01-21 21:12:53 +0530

    File name change updated in navlinks

--- src/components/navlinks.ts
+++ src/components/navlinks.ts
@@ -2,7 +2,7 @@ export const navLinks = [
   { text: "PSG TECH", link: "https://www.psgtech.edu", isBlank: true },
   { text: "Home", link: "#" },
   { text: "Seating Arrangement", link: "/assets/docs/seating-arrangement.xlsx", isBlank: true },
-  { text: "Brochure", link: "/assets/docs/brochure (2).pdf", isBlank: true },
+  { text: "Brochure", link: "/assets/docs/brochure_2026.pdf", isBlank: true },
   { text: "Syllabus", link: "/assets/docs/syllabus.pdf", isBlank: true },
   { text: "Instructions", link: "/assets/docs/instructions.pdf", isBlank: true },
   { text: "Prize Pool", link: "/assets/docs/brochure.pdf", isBlank: true },
